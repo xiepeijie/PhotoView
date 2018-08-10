10d86e42fda485150c9c13db995f8bb514c5c66a
xiepeijie <xiepeijie@evergrande.cn>
2018-08-10 12:28:58 +0800

    update demo

--- app/src/main/java/me/payge/photoview/demo/MainActivity.java
+++ app/src/main/java/me/payge/photoview/demo/MainActivity.java
@@ -1,7 +1,10 @@
-package me.payge.photoview;
+package me.payge.photoview.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import me.payge.photoview.PhotoView;
+import me.payge.photoview.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PhotoView photoView = findViewById(R.id.photo);
+        photoView.enable();
     }
 }
